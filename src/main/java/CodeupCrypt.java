5167b53eacfa680b9883def1d0efb35bbf121332
Fernando Mendoza <fernandoza@gmail.com>
2019-12-16 11:02:19 -0600

    feat: create the encryption solution and tests to validate them

--- src/main/java/CodeupCrypt.java
+++ src/main/java/CodeupCrypt.java
@@ -1,5 +1,29 @@
 public class CodeupCrypt {
 
     public static String hash = "";
+    String[] vowels = {"a", "e", "i", "o", "u"};
+
+    public static String checkPassword(){
+
+        String result = "";
+
+        for (String letter: hash.split("")) {
+            // f
+            if(letter.equalsIgnoreCase("e")){
+                result += "3";
+            } else if(letter.equalsIgnoreCase("o")){
+                result += "0";
+            } else if(letter.equalsIgnoreCase("u")){
+                result += "9";
+            } else if(letter.equalsIgnoreCase("a")){
+                result += "4";
+            } else if(letter.equalsIgnoreCase("i")){
+                result += "1";
+            } else {
+                result += letter;
+            }
+        }
+        return result;
+    }
 
 }
--- src/test/java/CodeupCryptTest.java
+++ src/test/java/CodeupCryptTest.java
@@ -1,19 +1,41 @@
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CodeupCryptTest {
 
+    @Before
+    public void setup(){
+        CodeupCrypt.hash = "fer";
+    }
+
     @Test
     public void testIfHashCanBeChanged(){
-
-        assertEquals("", CodeupCrypt.hash);
-        CodeupCrypt.hash = "fer";
         assertNotEquals("", CodeupCrypt.hash);
         assertNotNull(CodeupCrypt.hash);
         assertEquals("fer", CodeupCrypt.hash);
-
     }
 
 
+    @Test
+    public void testIfTheEncryptionWorks(){
+        assertEquals("f3r", CodeupCrypt.checkPassword());
+
+        CodeupCrypt.hash = "FER";
+        assertEquals("F3R", CodeupCrypt.checkPassword());
+
+        CodeupCrypt.hash = "codeup";
+        assertEquals("c0d39p", CodeupCrypt.checkPassword());
+
+        CodeupCrypt.hash = "aeiou";
+        assertEquals("43109", CodeupCrypt.checkPassword());
+
+        CodeupCrypt.hash = "qwrty";
+        assertEquals("qwrty", CodeupCrypt.checkPassword());
+
+        CodeupCrypt.hash = "123$%";
+        assertEquals("123$%", CodeupCrypt.checkPassword());
+
+    }
 
 }
