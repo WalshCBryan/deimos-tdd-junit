1f7763379c4fd8c322817c25461f09969e09dc2a
Bryan C Walsh <Walsh.C.Bryan@Gmail.com>
2019-12-17 09:47:16 -0600

    improved tests

--- src/test/java/KitchenTest.java
+++ src/test/java/KitchenTest.java
@@ -55,4 +55,5 @@ public class KitchenTest {
         assertFalse(kitchenWithMany.contains("blue corn"));
     }
 
+
 }
--- src/test/java/StudentTest.java
+++ src/test/java/StudentTest.java
@@ -1,3 +1,4 @@
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -5,37 +6,50 @@ import java.util.ArrayList;
 import static org.junit.Assert.*;
 
 public class StudentTest {
+
+    Student fer;
+Student nullStudent;
+    Student studentWithManyGrades;
+
+    @Before
+    public void init(){
+        Student nullStudent = null;
+        fer = new Student(1L, "fer");
+        fer.addGrade(80);
+        studentWithManyGrades= new Student(2L, "trant");
+        studentWithManyGrades.addGrade(80);
+        studentWithManyGrades.addGrade(90);
+        studentWithManyGrades.addGrade(100);
+    }
+
+
     @Test
     public void testCreateStudent(){
-        Student fer = new Student(1L, "fer");
-        Student ryan = null;
-        assertNull(ryan);
+        assertNull(nullStudent);
         assertNotNull(fer);
+        assertEquals("fer", fer.getName());
+        assertEquals(1, fer.getId());
     }
 
     @Test
     public void testStudentFields(){
-        Student fer = new Student(1L, "fer");
         assertSame(1L, fer.getId());
         assertSame("fer", fer.getName());
-        assertSame(0, fer.getGrades().size());
+        assertSame(1, fer.getGrades().size());
     }
 
 
     @Test
     public void testAddGrade(){
-        Student fer = new Student(1L, "fer");
-        fer.addGrade(100);
-        assertSame(100, fer.getGrades().get(0));
-        fer.addGrade(80);
-        assertSame(80, fer.getGrades().get(1));
+        assertSame(80, fer.getGrades().get(0));
+        assertEquals(1,fer.getGrades().size(),0);
+        assertSame(90, studentWithManyGrades.getGrades().get(1));
+        assertEquals(3,studentWithManyGrades.getGrades().size(),0);
     }
 
     @Test
     public void testAverageGrade(){
-        Student fer = new Student(1L, "fer");
-        fer.addGrade(100);
-        fer.addGrade(80);
-        assertEquals(90, fer.getGradeAverage(), 0);
+        assertEquals(80, fer.getGradeAverage(), 0);
+        assertEquals(90, studentWithManyGrades.getGradeAverage(), 0);
     }
 }
